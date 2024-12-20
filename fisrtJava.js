fa745948291327d9ffb63ab8889e8ddbaa9f422a
Esteban Mendia <estebanmendia55@hotmail.com>
2024-12-20 11:35:20 -0500

    Commit inicial

---
+++ fisrtJava.js
@@ -0,0 +1,10 @@
+function sumarNumeros(num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Ejemplo de uso
+  let numero1 = 5;
+  let numero2 = 3;
+  let resultado = sumarNumeros(numero1, numero2);
+  
+  console.log("La suma de los dos números es: " + resultado);
